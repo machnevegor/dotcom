0eff1bc57021dfa2f128094fb1eec984efc1c729
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2022-03-09 09:41:50 +0300

    Improved markdown...

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,6 @@
 import type { APIHandler } from "aleph/types";
 
 export const handler: APIHandler = ({ response }) => {
-  response.json(
-    "hi! my name is Egor and I'm 17. I love coding, so " +
-      "check out my [github](https://github.com/machnevegor).",
-  );
+  response.body = "hi! my name is Egor and I'm 17. I love coding, " +
+    "so check out my [github](https://github.com/machnevegor).";
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Meta from "~/components/layout/meta.tsx";
 import Terminal from "~/components/terminal/terminal.tsx";
 import styles from "~/style/home.module.css";
 
-import { parse } from "marked";
+import Markdown from "marked-react";
 
 const title = "Egor Machnev";
 const description = "🐍Full stack developer in " +
@@ -14,14 +14,13 @@ const resume = "https://docs.google.com/document/d/" +
   "1o8XloGaqtCceoNmCR07Pc0VSTYqxf1vgp0W8yPdT9tc";
 
 export default function Home() {
-  const [result, setResult] = useState("<em>...</em>");
+  const [result, setResult] = useState("...");
 
   useEffect(() => {
     fetch("/api/hello")
-      .then((resp) => resp.json())
-      .then((markdown) => parse(markdown))
-      .then((marked) => setResult(marked))
-      .catch((e) => console.error(e));
+      .then((resp) => resp.text())
+      .then((md) => setResult(md))
+      .catch(console.error);
   }, []);
 
   return (
@@ -32,7 +31,7 @@ export default function Home() {
       </head>
       <main className={styles.page}>
         <Terminal filename="hello.ts">
-          <div dangerouslySetInnerHTML={{ __html: result }} />
+          <Markdown>{result}</Markdown>
         </Terminal>
         <div className={styles.links}>
           <a href="https://t.me/machnevegor">Telegram</a>
