b93b60c10c2907c60490daee95a1aa2260619603
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2022-03-09 23:52:34 +0300

    Minor changes...

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 ![machnevegor](https://raw.githubusercontent.com/machnevegor/dotcom/main/public/files/preview.png)
 
-Personal website written in [Deno](https://deno.land) and
-[Aleph.js](http://alephjs.org), automatically deploys to
-[Vercel](https://vercel.com).
+Personal website written in [Deno](https://deno.land/) and
+[Aleph.js](http://alephjs.org/), automatically deploys to
+[Vercel](https://vercel.com/).
 
 > https://machnevegor.com
 
 ### System Requirements
 
-- [Deno](https://deno.land) 1.13+
+- [Deno](https://deno.land/) 1.13+
 - [Aleph.js](https://deno.land/x/aleph) 0.3.0+
-- [VS Code](https://code.visualstudio.com) with
+- [VS Code](https://code.visualstudio.com/) with
   [deno extension](https://marketplace.visualstudio.com/items?itemName=denoland.vscode-deno)
   (recommended)
 
--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -4,14 +4,16 @@ interface MetaProps {
   title: string;
   description: string;
   keywords?: string[];
-  image?: string;
   card?: "summary" | "summary_large_image";
+  image?: string;
+  site?: string;
 }
 
 // default meta
 const keywords = ["machnevegor", "developer", "coding"];
-const image = "https://machnevegor.com/files/card.png";
 const card = "summary_large_image";
+const image = "https://machnevegor.com/files/card.png";
+const site = "@machnevegor";
 
 export default function Meta(
   { title, description, ...meta }: MetaProps,
@@ -24,11 +26,11 @@ export default function Meta(
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
       <meta name="og:image" content={meta.image || image} />
+      <meta name="og:url" content="https://machnevegor.com/" />
 
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={meta.image || image} />
       <meta name="twitter:card" content={meta.card || card} />
+      <meta name="twitter:image" content={meta.image || image} />
+      <meta name="twitter:site" content={meta.site || site} />
     </head>
   );
 }
