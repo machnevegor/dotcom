b91e9a22a535b583c4576659f08f0936ee32a793
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2022-03-10 07:04:28 +0300

    Update layout.tsx

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
 export default function Layout(
-  { children }: LayoutProps,
+  { children }: { children: ReactNode },
 ) {
   return (
     <>
