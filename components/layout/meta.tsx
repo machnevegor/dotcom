de2b0657bb795874b37b52f21f6a1a77c3c43ac5
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2022-03-12 19:36:25 +0300

    Update meta.tsx

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -3,38 +3,38 @@ import React, { FC } from "react";
 interface MetaProps {
   title: string;
   description: string;
-  keywords?: string[];
-  card?:
-    | "summary"
-    | "summary_large_image"
-    | "player"
-    | "app";
   image?: string;
+  card?: string;
 }
 
 // default meta
-const keywords = ["machnevegor", "developer"];
-const card = "summary";
 const image = "https://machnevegor.com/logos/icon.png";
+const card = "summary";
 
 const Meta: FC<MetaProps> = (
   { title, description, ...meta },
 ) => {
   return (
     <head>
+      {/* title */}
       <title>{title}</title>
-
-      <meta name="description" content={description} />
-      <meta name="keywords" content={(meta.keywords || keywords).join(",")} />
-
       <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="og:image" content={meta.image || image} />
-      <meta name="og:site_name" content="machnevegor.com" />
 
-      <meta name="twitter:card" content={meta.card || card} />
+      {/* description */}
+      <meta name="description" content={description} />
+      <meta name="og:description" content={description} />
+
+      {/* image */}
+      <meta name="og:image" content={meta.image || image} />
       <meta name="twitter:image" content={meta.image || image} />
+
+      {/* url */}
+      <meta name="og:url" content="https://machnevegor.com/" />
       <meta name="twitter:site" content="@machnevegor" />
+
+      {/* type */}
+      <meta name="og:type" content="profile" />
+      <meta name="twitter:card" content={meta.card || card} />
     </head>
   );
 };
