16e296f10a536ed4c82edffd67c312ee7e9364bb
Egor Machnev <egorikhelp@gmail.com>
2022-08-06 19:59:03 +0300

    simplify

--- main.ts
+++ main.ts
@@ -3,11 +3,10 @@ import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
 blog({
   title: "Egor Machnev",
   description: "machnevegor",
-  avatar: "./assets/icon.svg",
+  avatar: "./logo.png",
   author: "Egor Machnev",
   links: [
     { title: "Email", url: "mailto:egorikhelp@gmail.com" },
     { title: "GitHub", url: "https://github.com/machnevegor" },
   ],
-  favicon: "./assets/logo.svg",
 });
