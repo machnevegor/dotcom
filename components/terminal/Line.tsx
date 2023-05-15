2b85ccbead4f4c5189795fe7eaab6aef3003360a
machnevegor <egorikhelp@gmail.com>
2023-05-16 01:19:18 +0300

    chore: add an idle command line

--- components/terminal/Line.tsx
+++ components/terminal/Line.tsx
@@ -1,4 +1,4 @@
-export default function Line({ children }: { children: React.ReactNode }) {
+export default function Line({ children }: { children?: React.ReactNode }) {
   return (
     <p className="font-mono text-xl">
       <span className="color-blue">machnevegor</span> ~/ {children}
