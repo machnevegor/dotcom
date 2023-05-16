2944f8bb7e36490d734b51c8ef3e86b6a1fca8c4
machnevegor <egorikhelp@gmail.com>
2023-05-16 12:19:57 +0300

    feat: update meta in index.html

--- components/terminal/Line.tsx
+++ components/terminal/Line.tsx
@@ -1,6 +1,6 @@
 export default function Line({ children }: { children?: React.ReactNode }) {
   return (
-    <p className="font-mono text-xl">
+    <p className="font-mono text-xl select-none">
       <span className="color-blue">machnevegor</span> ~/ {children}
     </p>
   );
