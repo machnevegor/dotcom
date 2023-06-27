18be2e49ca77cfcecf1e5329f922163a8f40268f
machnevegor <egorikhelp@gmail.com>
2023-06-27 10:18:31 +0300

    feat: update bio

--- components/program/Bio.tsx
+++ components/program/Bio.tsx
@@ -4,14 +4,16 @@ export default function Bio({ onDone }: { onDone?: () => void }) {
   return (
     <Program command="cat README.md" onStart={onDone}>
       <p className="font-mono text-xl">
-        I have two years of commercial experience in frontend and backend
-        development.
+        I am a Software Engineer since 2018. I have two years of commercial
+        experience in both front- and back-end development.
       </p>
       <br />
-      <p className="font-mono text-xl">TypeScript • Python • Rust</p>
+      <p className="font-mono text-xl">- TypeScript</p>
+      <p className="font-mono text-xl">- Python</p>
+      <p className="font-mono text-xl">- Rust</p>
       <br />
       <p className="font-mono text-xl">
-        I do graph theory, decentralized data execution and storage. Currently I
+        I do graph theory, decentralized data execution and storage. Nowadays I
         am interested in machine learning.
       </p>
     </Program>
