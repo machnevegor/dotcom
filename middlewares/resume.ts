199e4dc936e932c33ae3754d521c9b030461e8c3
machnevegor <egorikhelp@gmail.com>
2024-03-15 09:49:18 +0300

    chore: :art: clean up

--- middlewares/resume.ts
+++ middlewares/resume.ts
@@ -8,15 +8,15 @@ async function pdf(filepath: string): Promise<Response> {
 }
 
 export default <Middleware> {
-  async fetch(req, ctx) {
+  fetch(req, ctx) {
     const { pathname } = new URL(req.url);
 
     switch (pathname) {
       case "/resume":
-        return await pdf("./assets/resume.pdf");
+        return pdf("./assets/resume.pdf");
       case "/резюме":
       case "/%D1%80%D0%B5%D0%B7%D1%8E%D0%BC%D0%B5": // Windows-1251
-        return await pdf("./assets/резюме.pdf");
+        return pdf("./assets/резюме.pdf");
     }
 
     return ctx.next();
